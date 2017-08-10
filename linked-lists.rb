2ba8b8b570976aba37c96a48cf651ab11e985504
Gligor <dracevo.kromid@gmail.com>
2017-08-10 16:34:32 +0200

    Add everything left, add tests, tests pass, final commit

--- linked-lists.rb
+++ linked-lists.rb
@@ -1,6 +1,8 @@
 #Linked lists project from theodinproject.com
 #https://www.theodinproject.com/courses/ruby-programming/lessons/linked-lists
 
+#note- find > problem alert for things i need to remember
+
 class LinkedList
 
   def initialize
@@ -14,7 +16,7 @@ class LinkedList
   class Node
     attr_accessor :value, :next_node
 
-    def initialize(value=nil, next_node=nil) #maybe I'll need to add code here
+    def initialize(value=nil, next_node=nil)
       @value = value
       @next_node = next_node
     end
@@ -24,18 +26,18 @@ class LinkedList
   def append
     new_node = Node.new
 
-    if @head.nil? #aka list is empty-no nodes at all
+    if @head.nil? #aka empty list (no nodes)
       @head = new_node
       @tail = new_node
       @head.next_node = @tail
-    #potentially add the elsif (head not nil, tail nil) back
+      @tail.next_node = nil #problem alert
     else
       @tail.next_node = new_node
       @tail = new_node
+      @tail.next_node = nil #problem alert
     end
   end
 
-  end
 
   #add a new node at start
   def prepend
@@ -45,6 +47,7 @@ class LinkedList
       @head = new_node
       @tail = new_node
       @head.next_node = @tail
+      @tail.next_node = nil
     else
       new_node.next_node = @head
       @head = new_node
@@ -55,10 +58,10 @@ class LinkedList
   def size
     if head.nil?
       return 0
-    else #or just this below
+    else
       count = 1 # remember, array with indexes 0,1,2,3 is size 4 !
       x = @head
-      until x.nil? #took out x.next_node.nil?, check later
+      until x.next_node.nil? #problem alert
         count += 1
         x = x.next_node
       end
@@ -77,24 +80,13 @@ class LinkedList
     return @tail
   end
 
-  #return node at given index (?)
-  #can be done with size too, same shit
+  #return node at given index
   def at(index)
-    count = 0
-
-    x = @head
-    while true #possible work here later
+    self.traverse_list_with_count do |node, count|
       if count == index
-        return x #problem alert
-        return
-      end
-      count += 1
-
-      if x.next_node.nil?
+        return node
+      elsif count > self.size
         print "A node at the given index doesn't exist"
-        return
-      else
-        x = x.next_node
       end
     end
   end
@@ -118,50 +110,118 @@ class LinkedList
     end
   end
 
-  end
-
   #returns the index of the node whose data(value valjda?) matches the given, else nil
   def find(data)
-    #note-there may be a way to extract the count value from traverse_list
-    #so I don't need to make a new index counter, but I don't know it
-    index = 0
-    self.traverse_list do |node|
+    self.traverse_list_with_count do |node, index|
       return index if node.value == data
-      index += 1
     end
   end
 
   #prints the list elements as strings, format ( data ) -> ( data ) -> ( data ) -> nil
   def to_s
     self.traverse_list do |node|
-      print "( #{node.data} ) -> "
+      print "( #{node.value} ) -> "
     end
     print "nil" #because of how traverse_list works this has to be this way
   end
 
   #extra credit
-  #adds node at the given index
-  def insert_at(index)
-    prev = self.at(index) #idk how to call this....
-    post = self.at(index+1)
 
-
+  #IMPORTANT - head is index 0, tail is last index
+  #suport function for insert_at and remove_at
+  #IMPORTANT- if i find a way to call self.at for two different selfs this is not needed
+  def get_index(index)
+    return self.at(index)
   end
-  def remove_at(index); end #removes node at the given index
+
+  #adds node at the given index
+  def insert_at(index, insertion)
+    prev = get_index(index)
+    post = get_index(index+1)
+
+    #if index of insertion is bigger than list size it will insert at the end
+    if prev.nil?
+      @tail.next_node = insertion
+      @tail = insertion #problem alert
+      @tail.next_node = nil
+    else
+      prev.next_node = insertion
+      insertion.next_node = post
+    end
+  end
+
+  #removes node at the given index
+  def remove_at(index)
+    if index == 0
+      @home = get_index(index+1)
+      @home.next_node = get_index(index+2)
+    elsif get_index(index) == @tail
+      @tail = get_index(index-1)
+      @tail.next_node = nil
+    else
+      prev = get_index(index-1)
+      post = get_index(index+1)
+      prev.next_node = post
+    end
+  end
 
   #extra function for convenience
 
   #go through the list, start to finish (works with a block)
   def traverse_list
     return self unless block_given?
-    count = 0 #technically not needed because i don't know how to "extract" it for outside-of-function-use
     x = @head
     until x.nil?
-      count += 1
       yield x
       x = x.next_node
     end
   end
 
+  #yields x and count
+  def traverse_list_with_count
+    return self unless block_given?
+    count = 0
+    x = @head
+    until x.nil?
+      yield x, count
+      count += 1
+      x = x.next_node
+    end
+  end
+
+  def create_node(value)
+    return Node.new(value)
+  end
+
 end
 list = LinkedList.new
+list.append
+list.append
+list.append
+puts list.to_s
+puts print "size-", list.size
+new_node = list.create_node("123")
+new_node2 = list.create_node("456")
+list.insert_at(0, new_node)
+puts list.to_s
+puts print "size-", list.size
+list.insert_at(2, new_node2)
+puts list.to_s
+puts print "size-", list.size
+list.remove_at(3)
+puts list.to_s
+puts print "size-", list.size
+list.prepend
+puts list.to_s
+puts print "size-", list.size
+list.pop
+puts list.to_s
+puts print "size-", list.size
+puts list.head
+puts list.at(0)
+puts list.tail
+puts list.at(3)
+puts list.find("123")
+puts list.find("456")
+
+#everything works as expected...gg
